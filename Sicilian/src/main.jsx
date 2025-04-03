11c003a63e8cd49fc4e786610e9e2d4e0c04fc17
Williamho4 <willyho_@hotmail.se>
2025-04-03 13:57:48 +0200

    Added UserContext and WhoAmI request

--- Sicilian/src/main.jsx
+++ Sicilian/src/main.jsx
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import { router } from "./router";
 import { RouterProvider } from "react-router-dom";
-import './index.css'
+import "./index.css";
+import { UserProvider } from "../context/UserContext";
 
-
-createRoot(document.getElementById('root')).render(
- 
+createRoot(document.getElementById("root")).render(
+  <UserProvider>
     <RouterProvider router={router} />
- 
-)
+  </UserProvider>
+);
--- Sicilian/src/views/LoggaIn.jsx
+++ Sicilian/src/views/LoggaIn.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 export const LoggaIn = () => {
-    return (
-        <section>
-            <h1>Logga in</h1>
-        </section>
-    );
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginUser = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3000/user/login", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        console.log("Login successful:", data);
+        setUsername("");
+        setPassword("");
+      } else {
+        console.log("wrong credentials");
+      }
+    } catch (error) {
+      console.error("Request failed:", error);
+    }
+  };
+
+  return (
+    <section>
+      <form action="submit" onSubmit={(e) => loginUser(e)}>
+        <label htmlFor="">username</label>
+        <input type="text" onChange={(e) => setUsername(e.target.value)} />
+        <label htmlFor="">password</label>
+        <input type="text" onChange={(e) => setPassword(e.target.value)} />
+        <button type="submit">Log In</button>
+      </form>
+    </section>
+  );
 };
