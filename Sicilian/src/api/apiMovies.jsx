2258e064f996aa76d49eb564cce141d9b164eeac
Adrian Jönsson <adrianjonsson@hotmail.com>
2025-04-03 09:49:19 +0200

    getMovieById

---
+++ Sicilian/src/api/apiMovies.jsx
@@ -0,0 +1,17 @@
+import { apiRequest } from './apiRequest';
+
+export const getMovies = (errorMessage = 'Fel vid hämtning av filmer') =>
+  apiRequest('http://localhost:3000/movies', {}, errorMessage);
+
+export const getMovieById = (movieId, errorMessage = 'Fel vid hàng av filmer') =>
+  apiRequest(`http://localhost:3000/movies/${movieId}`, {}, errorMessage);
+
+export const deleteMovie = (
+  movieId,
+  errorMessage = 'Fel vid radering av film'
+) =>
+  apiRequest(
+    `http://localhost:3000/movies/${movieId}`,
+    { method: 'DELETE' },
+    errorMessage
+  );
