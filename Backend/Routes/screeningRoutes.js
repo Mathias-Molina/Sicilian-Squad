41d4d16a5f7fdb62899ffee8b4490638aaf60828
Williamho4 <willyho_@hotmail.se>
2025-04-07 11:19:50 +0200

    Moved admin middleware

--- Backend/Routes/screeningRoutes.js
+++ Backend/Routes/screeningRoutes.js
@@ -6,16 +6,12 @@ import {
 } from "../Controllers/screeningsController.js";
 import isAdmin from "../Middleware/isAdmin.js";
 import authMiddleware from "../Middleware/auth.js";
+main;
 
 export const screeningRouter = express.Router();
 
-screeningRouter.get("/", authMiddleware, isAdmin, getAllScreeningsHandler);
-screeningRouter.get(
-  "/:movie_id",
-  authMiddleware,
-  isAdmin,
-  getScreeningsByMovieIdHandler
-);
-screeningRouter.post("/add", addScreeningsHandler);
+screeningRouter.get("/", getAllScreeningsHandler);
+screeningRouter.get("/:movie_id", getScreeningsByMovieIdHandler);
+screeningRouter.post("/add", authMiddleware, isAdmin, addScreeningsHandler);
 
 screeningRouter.get("/", getAllScreeningsHandler);
