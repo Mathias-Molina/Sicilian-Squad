8caec4ff796e875f169308db2ac2e8de2ee3b3b0
Dalavan123 <dalavan@hotmail.com>
2025-04-07 15:12:33 +0200

    Påbörjat apiBookings i Frontend med getBookingsByUserId

--- Sicilian/src/api/apiBookings.jsx
+++ Sicilian/src/api/apiBookings.jsx
@@ -1,1 +1,4 @@
-i
+import { apiRequest } from './apiRequest';
+
+export const getBookingsByUserId = (errorMessage = 'Fel vid hitta bokningar') =>
+  apiRequest(`http://localhost:3000/bookings/user/${userId}`, {}, errorMessage);
