5a3b721ed8678640119af8c6019424bea963f3bc
Williamho4 <willyho_@hotmail.se>
2025-04-07 15:30:01 +0200

    add screening page done

--- Sicilian/src/api/apiScreenings.jsx
+++ Sicilian/src/api/apiScreenings.jsx
@@ -11,3 +11,26 @@ export const getScreenings = (
     },
     errorMessage
   );
+
+export const addScreenings = (
+  movie_id,
+  salon_id,
+  screening_time,
+  errorMessage = "Gick inte att lägga till visning"
+) =>
+  apiRequest(
+    `http://localhost:3000/screenings/add`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        movie_id: Number(movie_id),
+        salon_id: Number(salon_id),
+        screening_time,
+      }),
+    },
+    errorMessage
+  );
--- Sicilian/src/views/AddScreening.jsx
+++ Sicilian/src/views/AddScreening.jsx
@@ -1,3 +1,103 @@
+import { useState, useEffect } from "react";
+import { addScreenings } from "../api/apiScreenings";
+import { getMovies } from "../api/apiMovies";
+import { getSalons } from "../api/apiSalons";
+
 export const AddScreening = () => {
-  return <div>HEllo</div>;
+  const [movie, setMovie] = useState(undefined);
+  const [salon, setSalon] = useState(undefined);
+  const [screeningTime, setScreeningTime] = useState("");
+  const [movies, setMovies] = useState(null);
+  const [salons, setSalons] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    getMovies()
+      .then((data) => {
+        setMovies(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message || "Något gick fel");
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    getSalons()
+      .then((data) => {
+        setSalons(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+      });
+  }, []);
+
+  const handleAddScreening = async (e, movie, salon, screening_time) => {
+    e.preventDefault();
+    setMessage(null);
+
+    if (!movie || !salon || !screening_time) {
+      return setError("All fields required");
+    }
+
+    try {
+      const response = await addScreenings(
+        movie.movie_id,
+        salon.salon_id,
+        screening_time
+      );
+      setMessage(
+        `${response.message} ${movie.movie_title} ${salon.salon_id} ${screeningTime}`
+      );
+      setError(null);
+      setMovie(undefined);
+      setSalon(undefined);
+      setScreeningTime("");
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  return (
+    <div>
+      {(error && (error.message || error)) || null}
+      {message && message}
+      <form
+        onSubmit={(e) => handleAddScreening(e, movie, salon, screeningTime)}
+      >
+        <p>{`Chosen movie: ${movie ? movie.movie_title : ""}`}</p>
+        <p>{`Chosen salon: ${salon ? salon.salon_name : ""}`}</p>
+        <input
+          type="datetime-local"
+          placeholder="screening_time"
+          value={screeningTime}
+          onChange={(e) => setScreeningTime(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <ul>
+        {movies &&
+          movies.map((movie) => (
+            <div key={movie.movie_id} onClick={() => setMovie(movie)}>
+              {movie.movie_title}
+              <img
+                src={movie.movie_poster}
+                alt={movie.movie_title}
+                width={"40px"}
+              />
+            </div>
+          ))}
+      </ul>
+      {salons &&
+        salons.map((salon) => (
+          <div key={salon.salon_id} onClick={() => setSalon(salon)}>
+            {salon.salon_name}
+          </div>
+        ))}
+    </div>
+  );
 };
