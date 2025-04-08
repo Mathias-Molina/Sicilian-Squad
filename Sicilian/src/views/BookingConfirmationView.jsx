38442580214bfbc826c6fe25ca939b6d2d36582e
Zhel <maricelrefama@gmail.com>
2025-04-08 10:55:11 +0200

    added a comment

--- Sicilian/src/views/BookingConfirmationView.jsx
+++ Sicilian/src/views/BookingConfirmationView.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom"; {/}
+import { useLocation } from "react-router-dom"; {/*Maricel*/ }
 
 export const BookingConfirmationView = () => {
     const { state } = useLocation();
