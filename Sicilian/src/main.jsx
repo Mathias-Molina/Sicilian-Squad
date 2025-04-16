a2cf21dc568d45a62e1c0c0434f49d09f7d874bf
Dalavan123 <dalavan@hotmail.com>
2025-04-16 10:01:33 +0200

    Flyttat in index.css i stylingmappen och ändrat bokningsnummer till unik

--- Sicilian/src/main.jsx
+++ Sicilian/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { router } from './router';
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
+import './styling/index.css';
 import { UserProvider } from './context/UserContext';
 
 createRoot(document.getElementById('root')).render(
