c2a4c8579d9d02c77ae66573797a7a7ba615c69c
Zhel <maricelrefama@gmail.com>
2025-04-17 20:44:46 +0200

    deleted footer files

--- Sicilian/src/components/App.jsx
+++ Sicilian/src/components/App.jsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
-import  Footer  from "./Footer";
-
+import "../styling/App.css";
 export function App() {
   return (
-    <div className = "wrapper">
+    <>
       <Navbar />
-      <div className = "content">
-        <Outlet />
-      </div>
-      <Footer />
-    </ div>
+      <Outlet />
+    </ >
   );
-}
+};
