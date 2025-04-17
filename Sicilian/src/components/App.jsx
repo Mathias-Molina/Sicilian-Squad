735d91f3fc46c9dfd5abcd63371a024d1db55096
Zhel <maricelrefama@gmail.com>
2025-04-17 12:39:54 +0200

    ONGOING fixing the footer

--- Sicilian/src/components/App.jsx
+++ Sicilian/src/components/App.jsx
@@ -1,14 +1,15 @@
-import "../styling/App.css";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 import  Footer  from "./Footer";
 
 export function App() {
   return (
-    <>
+    <div className = "wrapper">
       <Navbar />
-      <Outlet />
+      <div className = "content">
+        <Outlet />
+      </div>
       <Footer />
-    </>
+    </ div>
   );
 }
--- Sicilian/src/components/Footer.jsx
+++ Sicilian/src/components/Footer.jsx
@@ -1,34 +1,33 @@
-import "../styling/Footer.css"
+
 const Footer = () => {
-  return (
-    <footer>
-        <h3>Sicilian Squad</h3>
-        
-          <div className="footer-icons">
-              {/* Instagram Icon */}
-              <a href="https://instagram.com" target="_blank" rel="noreferrer" aria-label="Instagram">
-                  <i className="fab fa-instagram"></i>
-              </a>
 
-              {/* Facebook Icon */}
-              <a href="https://facebook.com" target="_blank" rel="noreferrer" aria-label="Facebook">
-                  <i className="fab fa-facebook-f"></i>
-              </a>
+    return (
+            <footer>
+                <h3>Sicilian Squad</h3>
+                <div className="footer-icons">
+                    {/* Instagram Icon */}
+                    <a href="https://instagram.com" target="_blank" rel="noreferrer" aria-label="Instagram">
+                        <i className="fab fa-instagram"></i>
+                    </a>
 
-              {/* GitHub Icon */}
-              <a href="https://github.com" target="_blank" rel="noreferrer" aria-label="GitHub">
-                  <i className="fab fa-github"></i>
-              </a>
+                    {/* Facebook Icon */}
+                    <a href="https://facebook.com" target="_blank" rel="noreferrer" aria-label="Facebook">
+                        <i className="fab fa-facebook-f"></i>
+                    </a>
 
-              <div className="footer-nav">
-                  <a href="/">Home</a>
-                  <a href="/contact">Contact Us</a>
-              </div>
+                    {/* GitHub Icon */}
+                    <a href="https://github.com" target="_blank" rel="noreferrer" aria-label="GitHub">
+                        <i className="fab fa-github"></i>
+                    </a>
 
-          </div>
-          <p>Copyright &copy; 2025; Designed by: <strong>Sicilian Squad</strong></p>
-    </footer>
-  )
-}
+                    <div className="footer-nav">
+                        <a href="/">Home</a>
+                        <a href="/contact">Contact Us</a>
+                    </div>
+                </div>
+                <p>Copyright &copy; 2025; Designed by: <strong>Sicilian Squad</strong></p>
+            </footer>
+    );
+};
 
-export default Footer
+export default Footer;
