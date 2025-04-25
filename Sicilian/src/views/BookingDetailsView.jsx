d0f0d76583927b5d47861f409a83eac5c37a4281
TheBluePrinter <mathiasmolina95@gmail.com>
2025-04-25 13:24:37 +0200

    tagit bort import modul från bookingdetailsview för att inte se stegräknare i redan bokade bokningar

--- Sicilian/src/views/BookingDetailsView.jsx
+++ Sicilian/src/views/BookingDetailsView.jsx
@@ -1,12 +1,10 @@
 import { useParams } from "react-router-dom";
 import { BookingCards } from "../components/BookingCards";
-import { StepIndicator } from "../components/StepIndicator";
 
 export const BookingDetailsView = () => {
   const { bookingNumber } = useParams();           
   return (
     <div>
-      <StepIndicator currentStep={5} />
       <BookingCards bookingNumber={bookingNumber} />  
     </div>
   );
