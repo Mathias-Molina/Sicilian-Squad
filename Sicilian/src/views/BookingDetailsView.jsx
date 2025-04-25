77a6b76ba17ca2ac12f0e2a17dcd2bd679cfb628
Adrian Jönsson <adrianjonsson@hotmail.com>
2025-04-25 14:45:57 +0200

    Försökt lösa Så stepIndikator bara syns på första bokning

--- Sicilian/src/views/BookingDetailsView.jsx
+++ Sicilian/src/views/BookingDetailsView.jsx
@@ -1,13 +1,28 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { BookingCards } from "../components/BookingCards";
 import { StepIndicator } from "../components/StepIndicator";
 
 export const BookingDetailsView = () => {
-  const { bookingNumber } = useParams();           
+  const { bookingNumber } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [showStepperOnce, setShowStepperOnce] = useState(
+    location.state?.fromNewBooking === true
+  );
+
+  useEffect(() => {
+    if (location.state?.fromNewBooking) {
+      navigate(location.pathname, { replace: true, state: {} });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
-      <StepIndicator currentStep={5} />
-      <BookingCards bookingNumber={bookingNumber} />  
+      {showStepperOnce && <StepIndicator currentStep={5} />}
+      <BookingCards bookingNumber={bookingNumber} />
     </div>
   );
 };
