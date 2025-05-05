2acf6122c93b0a909ebeb6db00d98eacd68d629b
Williamho4 <willyho_@hotmail.se>
2025-05-05 09:18:38 +0200

    fixed bug

--- Sicilian/src/components/FlipView.jsx
+++ Sicilian/src/components/FlipView.jsx
@@ -2,8 +2,8 @@ export function FlipView({ flipped, front, back }) {
   return (
     <div className={`flip-container ${flipped ? "flipped" : ""}`}>
       <div className="flipper">
-        <div className="front">{front}</div>
-        <div className="back">{back}</div>
+        <div className={`front ${flipped && "hide"}`}>{front}</div>
+        <div className={`back ${!flipped && "hide"}`}>{back}</div>
       </div>
     </div>
   );
